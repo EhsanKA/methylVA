78c0ad73bf4f03b9bf68b7e13ba66e01f218a9c8
EhsanKA <e.karimiara@gmail.com>
2024-10-18 17:24:31 +0200

    train vae on different subsets of data 20000 features

---
+++ train_v2_m1_vae_sub_data.py
@@ -0,0 +1,308 @@
+import os
+import time
+import random
+import numpy as np
+import pandas as pd
+import torch
+import argparse
+from pathlib import Path
+from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from torch.utils.data import DataLoader, TensorDataset
+import pytorch_lightning as pl
+import torch.nn as nn
+import torch.nn.functional as F
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning import Trainer, loggers
+
+# Set the environment variable inside the script
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
+
+
+# Set random seed for reproducibility
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # If using multi-GPU
+    torch.backends.cudnn.deterministic = True  # Ensures deterministic behavior
+    torch.backends.cudnn.benchmark = False  # Disables fast auto-tuning
+
+
+# Time tracking decorator
+def time_tracker(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        print(f"Starting {func.__name__}...")
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time:.2f} seconds to execute.")
+        return result
+    return wrapper
+
+
+class VAE(nn.Module):
+    def __init__(self, input_dim, latent_dim, hidden_dims=[2048,1024,512], dropout_rate=0.2):
+        super(VAE, self).__init__()
+        
+        # Encoder
+        self.encoder_layers = self.build_layers(input_dim, hidden_dims, dropout_rate)
+        # self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc_mu = nn.Linear(hidden_dims[-1], latent_dim)  # for mean
+        self.fc_logvar = nn.Linear(hidden_dims[-1], latent_dim)  # for log variance
+        
+        # Decoder
+        decoder_hidden_dims = hidden_dims[::-1]
+        self.decoder_layers =self.build_layers(latent_dim, decoder_hidden_dims, dropout_rate)
+        self.fc_output = nn.Linear(hidden_dims[0], input_dim)
+        # self.fc3 = nn.Linear(latent_dim, hidden_dim)
+        # self.fc4 = nn.Linear(hidden_dim, input_dim)
+
+    def build_layers(self, input_dim, hidden_dims, dropout_rate):
+        layers = []
+        for h_dim in hidden_dims:
+            layers.append(nn.Linear(input_dim, h_dim))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout_rate))
+            input_dim = h_dim
+        return nn.Sequential(*layers)
+    
+    def encode(self, x):
+        h = self.encoder_layers(x)
+        mu = self.fc_mu(h)
+        logvar = self.fc_logvar(h)
+        return mu, logvar
+    
+    def reparameterize(self, mu, logvar):
+        # Check if logvar has NaN or Inf values
+        if torch.isnan(logvar).any() or torch.isinf(logvar).any():
+            print(f"NaN or Inf detected in logvar: logvar={logvar}")
+        
+        # Clamp logvar to prevent extreme values
+        logvar = torch.clamp(logvar, min=-5, max=5)
+        
+        # Calculate std from logvar
+        std = torch.exp(0.5 * logvar)
+        
+        # Check if std has NaN or Inf values
+        if torch.isnan(std).any() or torch.isinf(std).any():
+            print(f"NaN or Inf detected in std computation: std={std}")
+        
+        # Sample from the latent space
+        eps = torch.randn_like(std)
+        z = mu + eps * std
+        
+        # Check if z has NaN or Inf values
+        if torch.isnan(z).any() or torch.isinf(z).any():
+            print(f"NaN or Inf detected in z computation: z={z}")
+        
+        return z
+
+    def decode(self, z):
+        h = self.decoder_layers(z)
+        # h = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc_output(h))
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+    def get_latent_embedding(self, x):
+        """
+        Method to get the latent embedding (the `z` vector) for an input.
+        """
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)  # this is the embedding
+        return z
+
+class VAE_Lightning(pl.LightningModule):
+    def __init__(self, input_dim=485577, latent_dim=128, hidden_dims=[2048, 1024, 512], dropout_rate=0.2, lr=1e-6):
+        super(VAE_Lightning, self).__init__()
+        
+        self.save_hyperparameters()  # Save hyperparameters for checkpointing
+
+        self.model = VAE(input_dim, latent_dim, hidden_dims, dropout_rate)
+        self.lr = lr
+    
+    def forward(self, x):
+        mu, logvar = self.model.encode(x)
+        z = self.model.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+    def get_latent_embedding(self, x):
+        return self.model.get_latent_embedding(x)
+        
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+
+        # Step 1: Create mask before replacing NaN values
+        mask = ~torch.isnan(x)  # mask where values are not NaN
+
+        # Step 2: Replace NaNs with zero or another neutral value for forward pass
+        x_filled = replace_nan_with_mean(x)
+        # x_filled = torch.nan_to_num(x, nan=0.0)
+
+        # Step 3: Pass through the model with filled values
+        z, mu, logvar = self.forward(x_filled)
+        x_hat, _, _ = self.model(x_filled)
+
+        # Step 4: Use the original x (with NaNs) and mask to calculate the loss
+        loss = self._vae_loss(x, x_hat, mu, logvar, mask)
+        print(f"Training loss: {loss.item()}")
+
+        self.log('train_loss', loss, on_step=False, on_epoch=True)
+        return loss
+
+    
+    def validation_step(self, batch, batch_idx):
+        x, _ = batch
+
+        # Step 1: Create mask before replacing NaN values
+        mask = ~torch.isnan(x)
+
+        # Step 2: Replace NaNs with zero or another neutral value for forward pass
+        x_filled = replace_nan_with_mean(x)
+        # x_filled = torch.nan_to_num(x, nan=0.0)
+
+        # Step 3: Pass through the model with filled values
+        z, mu, logvar = self.forward(x_filled)
+        x_hat, _, _ = self.model(x_filled)
+
+        # Step 4: Use the original x (with NaNs) and mask to calculate the loss
+        loss = self._vae_loss(x, x_hat, mu, logvar, mask)
+        print(f"Validation loss: {loss.item()}")
+
+        self.log('val_loss', loss, on_step=False, on_epoch=True)
+  
+
+    def _vae_loss(self, original_x, x_hat, mu, logvar, mask):
+        # Apply mask to ignore NaN values in the loss calculation
+        recon_loss = F.mse_loss(x_hat[mask], original_x[mask], reduction='mean')
+    
+        # Scale the KL divergence to balance the losses
+        kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        kl_loss = kl_loss / original_x.shape[0]  # Normalize by batch size or apply weighting
+    
+        return recon_loss + kl_loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters(), lr=self.lr)
+    
+
+from pytorch_lightning.callbacks import Callback
+
+class LossHistoryCallback(Callback):
+    def __init__(self):
+        super().__init__()
+        self.train_losses = []
+        self.val_losses = []
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        # Access the loss for the last training epoch from the logs
+        train_loss = trainer.callback_metrics.get('train_loss')
+        if train_loss is not None:
+            self.train_losses.append(train_loss.item())
+
+    def on_validation_epoch_end(self, trainer, pl_module):
+        # Access the loss for the last validation epoch from the logs
+        val_loss = trainer.callback_metrics.get('val_loss')
+        if val_loss is not None:
+            self.val_losses.append(val_loss.item())
+
+
+
+def replace_nan_with_mean(x):
+    # Calculate the column-wise mean, ignoring NaNs
+    col_mean = torch.nanmean(x, dim=0)
+    
+    # Find where NaN values are located
+    nan_mask = torch.isnan(x)
+    
+    # Replace NaNs with the corresponding column means
+    x[nan_mask] = torch.take(col_mean, nan_mask.nonzero()[:, 1])
+    
+    # Check if there are still NaN or Inf values
+    if torch.isnan(x).any() or torch.isinf(x).any():
+        print("NaN or Inf detected in the input data after imputation!")
+    
+    return x
+
+
+
+set_seed(42)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+
+
+
+parser = argparse.ArgumentParser(description="Train the VAE base on the sample size.")
+parser.add_argument("data_size", type=str, help="The size parameter to load the sampled data ('5000', '10000', '20000', '37067', 'shuffled_10000')")
+
+args = parser.parse_args()
+data_size = args.data_size
+
+dataset_path = 'data/v2/m1/'
+    
+# This loads both X (features) and y (labels) from the saved files
+train_tensors = torch.load("/".join([dataset_path, data_size, "train_dataset_tensors.pt"]))  # Loads (X_train, y_train)
+val_tensors = torch.load("/".join([dataset_path, data_size, "val_dataset_tensors.pt"]))      # Loads (X_val, y_val)
+test_tensors = torch.load("/".join([dataset_path, data_size, "test_dataset_tensors.pt"]))    # Loads (X_test, y_test)
+
+# Recreate the TensorDataset
+train_dataset = TensorDataset(*train_tensors)
+val_dataset = TensorDataset(*val_tensors)
+test_dataset = TensorDataset(*test_tensors)
+
+# Create DataLoaders
+batch_size = 16
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, worker_init_fn=lambda _: np.random.seed(42))
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, worker_init_fn=lambda _: np.random.seed(42))
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, worker_init_fn=lambda _: np.random.seed(42))
+
+# Create a logger
+logger = loggers.CSVLogger('lightning_logs/', name=f'v2_m1_vae_{data_size}')
+
+
+pl.seed_everything(42)
+
+# Initialize the VAE Lightning model
+input_dim = train_tensors[0].shape[1]  # The number of input features
+# input_dim = X_train_tensor.shape[1]  # The number of input features
+latent_dim = 256  # Latent dimension size, can be tuned
+hidden_dims = [2048, 1024, 512]
+dropout_rate = 0.2
+lr = 1e-6
+
+model = VAE_Lightning(
+    input_dim=input_dim,
+    latent_dim=latent_dim,
+    hidden_dims=hidden_dims,
+    dropout_rate=dropout_rate,
+    lr=lr)
+
+# Training
+loss_history_callback = LossHistoryCallback()
+checkpoint_callback = ModelCheckpoint(
+    monitor='val_loss',
+    save_top_k=1,
+    mode='min',
+    dirpath=f'{logger.save_dir}/{logger.name}/version_{logger.version}/checkpoints/',
+    filename='m1-vae-{epoch:02d}-{val_loss:.2f}'
+    )
+
+early_stopping_callback = EarlyStopping(monitor='val_loss', patience=3)
+
+trainer = pl.Trainer(
+    max_epochs=200,
+    gradient_clip_val=0.1,  # Clip gradients to avoid explosion
+    callbacks=[checkpoint_callback, early_stopping_callback, loss_history_callback],
+    precision=32,
+    accelerator='gpu',          # Use 'gpu' or 'cpu'
+    devices=1 if torch.cuda.is_available() else 'auto',  # Use 1 GPU or CPU ('auto' will pick the appropriate one)
+    deterministic=True,  # Ensure reproducibility
+    logger=logger
+)
+trainer.fit(model, train_loader, val_loader)
