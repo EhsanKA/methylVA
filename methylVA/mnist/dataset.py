2d135d3f4949c493e01049f3f4491d3ac85807f4
EhsanKA <e.karimiara@gmail.com>
2024-11-10 22:52:56 +0100

    added methyldata

--- methylVA/mnist/dataset.py
+++ methylVA/mnist/dataset.py
@@ -1,6 +1,10 @@
 import torch
 from torchvision import datasets
 from torchvision.transforms import v2
+import torch.utils.data
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
 
 def get_data_loaders():
     """
@@ -136,3 +140,81 @@ def get_gaussian_data_loaders():
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
+
+
+
+class MethylDataset(Dataset):
+    def __init__(self, data=None, metadata=None,
+                 pkl_file_data=None, pkl_file_metadata=None,
+                 transform=None
+                 ):
+        
+        if data is not None and metadata is not None:
+            self.data = data
+            self.metadata = metadata
+            self.labels = self.metadata['labels_encoded'].values
+        elif pkl_file_data is not None and pkl_file_metadata is not None:
+            self.data = pd.read_pickle(pkl_file_data)
+            # print(self.data.head())
+            # .astype('float32')
+            # self.data.set_index(self.data.columns[0], inplace=True)
+
+            self.metadata = pd.read_pickle(pkl_file_metadata)
+            self.labels = self.metadata['labels_encoded'].values
+        else:
+            raise ValueError("Either data and metadata or pkl files must be provided")
+            
+        self.transform = transform
+
+        # Check for unique problematic values
+        # Convert non-numeric values to NaN, keep them in self.data for inspection
+        self.data = self.data.apply(pd.to_numeric, errors='coerce')
+        self.data = np.round(self.data, decimals=4)
+
+        if self.data.isna().any().any():
+            print("Found NaN values in the data after conversion.")
+            # print(self.data[self.data.isna().any(axis=1)])
+
+
+    def __len__(self):
+        # Returns the number of samples
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # Get sample data
+        sample = self.data.iloc[idx, :].values
+
+        # Replace NaNs with 0.0 and convert to tensor
+        sample = np.where(np.isnan(sample), 0.0, sample)
+        sample = torch.tensor(sample, dtype=torch.float32)
+        sample = torch.where(torch.isinf(sample), torch.tensor(1.0, dtype=torch.float32), sample)
+        sample -= 0.5
+
+        label = torch.tensor(self.labels[idx], dtype=torch.long)
+
+        # Apply any transformations, if provided
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, label
+    
+
+
+def get_methyl_data_loaders(train_data_path,
+                            train_metadata_path,
+                            test_data_path,
+                            test_metadata_path,
+                            batch_size=128
+                            ):  
+    
+
+    # Create noise datasets
+    train_data = MethylDataset(pkl_file_data=train_data_path, pkl_file_metadata=train_metadata_path)
+    test_data = MethylDataset(pkl_file_data=test_data_path, pkl_file_metadata=test_metadata_path)
+
+    # Create data loaders
+    train_loader = DataLoader(train_data, batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
